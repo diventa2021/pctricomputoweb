18de10a70cee6c8251a4ced86d784f172d47820d
cristian zapata <km@cristians-Mac-mini.local>
2023-01-02 23:35:29 -0500

    project nextjs tricomputo

---
+++ next-pro/pages/events/index.js
@@ -0,0 +1,6 @@
+const Page = () => {
+    return (
+        <h1>Events</h1>
+    )
+}
+export default Page;
---
+++ next-pro/pages/events/producto1.js
@@ -0,0 +1,6 @@
+const Page = () => {
+    return (
+        <h1>Producto1</h1>
+    )
+}
+export default Page;
